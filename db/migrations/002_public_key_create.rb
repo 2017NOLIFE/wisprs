69efdebb81a0e7a8bed442d5d83a27151ecb09eb
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-04-13 14:09:48 +0800

    Public key migration refactored

--- db/migrations/002_public_key_create.rb
+++ db/migrations/002_public_key_create.rb
@@ -4,6 +4,7 @@ Sequel.migration do
   change do
     create_table(:public_keys) do
       primary_key :id
+      String :name, null: false
       String :key, null: false
       String :owner, null: false
     end
