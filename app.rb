1d88e53e5749d7b672b52143a6537689ce691748
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-04-13 21:59:53 +0800

    Basic routes created for Public key

--- app.rb
+++ app.rb
@@ -48,4 +48,40 @@ class WispersBase < Sinatra::Base
     status 201
     headers('Location' => [@request_url.to_s, saved_message.id].join('/'))
   end
+
+  get '/api/v1/public_keys/?' do
+    content_type 'application/json'
+
+    JSON.pretty_generate(data: Public_key.all)
+  end
+
+  get '/api/v1/public_keys/:id' do
+    content_type 'application/json'
+
+    public_key = Public_key.where("id = #{params[:id]}").first
+
+    if public_key
+      JSON.pretty_generate(data: public_key)
+    else
+      error_msg = "FAILED TO GET PUBLIC KEY: \"#{params[:id]}\""
+      logger.info error_msg
+      halt 404, error_msg
+    end
+  end
+
+  post '/api/v1/public_keys/?' do
+    content_type 'application/json'
+
+    begin
+      new_public_key = JSON.parse(request.body.read)
+      saved_public_key = Public_key.create(new_public_key)
+    rescue => e
+      error_msg = "FAILED to create a new public key: #{e.inspect}"
+      logger.info error_msg
+      halt 400, error_msg
+    end
+
+    status 201
+    headers('Location' => [@request_url.to_s, saved_public_key.id].join('/'))
+  end
 end
