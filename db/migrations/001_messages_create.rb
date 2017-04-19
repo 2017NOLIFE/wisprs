0731f9941a1f196f9cfc8080bbe6e7766a74989f
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-04-19 17:08:57 +0800

    Migrations refactored

--- db/migrations/001_messages_create.rb
+++ db/migrations/001_messages_create.rb
@@ -4,14 +4,20 @@ require 'sinatra'
 Sequel.migration do
   change do
     create_table(:messages) do
-      primary_key :id
+      String :id, type: :uuid, primary_key: true
+
       String :from, null: false
       String :to, null: false
       String :title, null: false
       String :about, null: false
       String :expire_date, null: false
       Bool :status, null: false, default: false
-      String :body, null: true
+
+      # secure data - initialize as nil until data provided
+      String :body_secure, text: true
+
+      DateTime :created_at
+      DateTime :updated_at
     end
   end
 end
--- db/migrations/002_public_key_create.rb
+++ db/migrations/002_public_key_create.rb
@@ -3,10 +3,14 @@ require 'sequel'
 Sequel.migration do
   change do
     create_table(:public_keys) do
-      primary_key :id
+      String :id, type: :uuid, primary_key: true
+      
       String :name, null: false
       String :key, null: false, unique: true
       String :owner, null: false
+
+      DateTime :created_at
+      DateTime :updated_at
     end
   end
 end
