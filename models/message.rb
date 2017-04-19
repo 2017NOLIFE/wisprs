91999cfedf4b48efb0ffdacf31636a68fb866239
ismaelnoble <ismaelnoble@gmail.com>
2017-04-20 00:36:16 +0800

    message table encrypt and decrypt

--- models/message.rb
+++ models/message.rb
@@ -2,11 +2,57 @@ require 'json'
 require 'base64'
 require 'sequel'
 
+require_relative '../lib/secure_db.rb'
+
 # Holds a Message's Information
 class Message < Sequel::Model
 	plugin :uuid, field: :id
 
 	#set_allowed_columns :title, :about
+
+	# encrypt field data functions
+	def from = ( from_plain )
+		self.from_secure = secureDB.encrypt(from_plain)
+	end
+
+	def to = ( to_plain )
+		self.to_secure = secureDB.encrypt(to_plain)
+	end
+
+	def title = ( title_plain )
+		self.to_secure = secureDB.encrypt(title_plain)
+	end
+
+	def about = (about_plain)
+		self.to_secure = secureDB.encrypt(about_plain)
+	end
+
+	def status = (status_plain)
+		self.to_secure = secureDB.encrypt(status_plain)
+	end
+
+	# decrypt field data functions
+	def from
+		secureDB.decrypt(from_secure)
+	end
+
+	def to
+		secureDB.decrypt(to_secure)
+	end
+
+	def title
+		secureDB.decrypt(title_secure)
+	end
+
+	def about
+		secureDB.decrypt(about_secure)
+	end
+
+	def status
+		secureDB.decryt(status_secure)
+	end
+
+# Json string
 
 	def to_json(options = {})
     JSON({
