d4b355b71f0da504f893e506cc7a5d55ccad9bda
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-04-21 00:13:01 +0800

    Migrations: secure added to fields names

--- db/migrations/001_messages_create.rb
+++ db/migrations/001_messages_create.rb
@@ -6,15 +6,15 @@ Sequel.migration do
     create_table(:messages) do
       String :id, type: :uuid, primary_key: true
 
-      String :from, null: false
-      String :to, null: false
-      String :title, null: false
-      String :about, null: false
+      String :from_secure, null: false
+      String :to_secure, null: false
+      String :title_secure, null: false
+      String :about_secure, null: false
       String :expire_date, null: false
-      Bool :status, null: false, default: false
+      String :status_secure, null: false, default: 'NO'
 
       # secure data - initialize as nil until data provided
-      String :body_secure, text: true
+      String :body_secure, null: false #, text: true
 
       DateTime :created_at
       DateTime :updated_at
--- db/migrations/002_public_key_create.rb
+++ db/migrations/002_public_key_create.rb
@@ -4,10 +4,10 @@ Sequel.migration do
   change do
     create_table(:public_keys) do
       String :id, type: :uuid, primary_key: true
-      
-      String :name, null: false
-      String :key, null: false, unique: true
-      String :owner, null: false
+
+      String :name_secure, null: false
+      String :key_secure, null: false, unique: true
+      String :owner_secure, null: false
 
       DateTime :created_at
       DateTime :updated_at
