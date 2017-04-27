e27ede47cd11891a89f9619a78245aa52f37f0ab
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-04-27 12:02:01 +0800

    Messages and public keys specs refactored

--- specs/messages_spec.rb
+++ specs/messages_spec.rb
@@ -2,44 +2,76 @@ require_relative './spec_helper'
 
 describe 'Testing Message resource routes' do
   before do
-    Message.dataset.delete
-    Public_key.dataset.delete
-    Account.dataset.delete
+    Public_key.dataset.destroy
+    Message.dataset.destroy
+    Account.dataset.destroy
+    @from_account = CreateAccount.call(
+      username: 'sender',
+      password: 'mypass',
+      email: 'sender@email.com'
+    )
+    @to_account = CreateAccount.call(
+      username: 'receiver',
+      password: 'mypass',
+      email: 'receiver@email.com'
+    )
   end
 
   describe 'Create a new message' do
-    it 'Happy: should create new message in db'do
-    req_header = { 'CONTENT_TYPE' => 'application/json' }
-    req_body = { from: 'me', to:'you',title:'a title',about:'an about',
-                             expire_date:'some value',status:'good',body: 'Demo message' }.to_json
-    post "/api/v1/messages/",
-          req_body, req_header
-    _(last_response.status).must_equal 201
-  end
+    before do
+      message_data = {
+        from_id: @from_account.id,
+        to_id: @to_account.id,
+        title: 'Hello',
+        about: 'All about greeting',
+        expire_date: 'Test',
+        status: 'NO',
+        body: 'Hello everyone'
+      }
+      @req_body = message_data.to_json
+    end
+    it 'Happy: should create new message in db' do
+      req_header = { 'CONTENT_TYPE' => 'application/json' }
+      post '/api/v1/messages/', @req_body, req_header
+      _(last_response.status).must_equal 201
+      _(last_response.location).must_match(%r{http://})
+    end
 
     it 'SAD: should not add a message for non-existant variable' do
+      message_data = {
+        from_id: @from_account.id,
+        to_id: @to_account.id,
+        body: 'Hello everyone'
+      }
+      @req_body = message_data.to_json
       req_header = { 'CONTENT_TYPE' => 'application/json' }
-      req_body = { body: 'bla bla bla testing test'}.to_json
-      post "/api/v1/messages/",
-            req_body, req_header
+      post '/api/v1/messages/', @req_body, req_header
       _(last_response.status).must_equal 400
       _(last_response.location).must_be_nil
     end
   end
 
   describe 'Getting messages' do
+    before do
+      @new_message = SendMessage.call(
+        from_id: @from_account.id,
+        to_id: @to_account.id,
+        title: 'Hello',
+        about: 'All about greeting',
+        expire_date: 'Test',
+        status: 'NO',
+        body: 'Hello everyone'
+      )
+    end
     it 'HAPPY: should find existing messages' do
-      message = Message.create(from: 'me', to:'you',title:'a title',about:'an about',
-                               expire_date:'some value',status:'good',body: 'Demo message')
-      get "/api/v1/messages/#{message.id}"
+      get "/api/v1/messages/#{@new_message.id}"
       _(last_response.status).must_equal 200
-      parsed_config = JSON.parse(last_response.body)['data']
-      _(parsed_config['type']).must_equal 'message'
+      results = JSON.parse(last_response.body)
+      _(results['data']['id']).must_equal @new_message.id
     end
 
     it 'SAD: should not find non-existant message' do
-      message_id = invalid_id(Message)
-      get "/api/v1/messages/#{message_id}"
+      get "/api/v1/messages/#{random_str(10)}"
       _(last_response.status).must_equal 404
     end
   end
