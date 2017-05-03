674ac0875b4387bf742dae9bc1d9416be3468fec
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-05-03 16:45:35 +0800

    Tests for user authentication

--- specs/accounts_spec.rb
+++ specs/accounts_spec.rb
@@ -78,4 +78,38 @@ describe 'Testing Account resource routes' do
       _(last_response.status).must_equal 401
     end
   end
+
+  describe 'Authenticating an account' do
+    before do
+      @new_account = CreateAccount.call(
+        username: 'test.name',
+        email: 'test@email.com', password: 'mypass'
+      )
+    end
+
+    it 'HAPPY: should authenticate an existing account' do
+      credentials = {
+        username: 'test.name',
+        password: 'mypass'
+      }
+      @req_body = credentials.to_json
+      req_header = { 'CONTENT_TYPE' => 'application/json' }
+      post '/api/v1/accounts/authenticate', @req_body, req_header
+      _(last_response.status).must_equal 200
+
+      results = JSON.parse(last_response.body)
+      _(results['account']['id']).must_equal @new_account.id
+    end
+
+    it 'SAD: should not authenticate, wrong password' do
+      credentials = {
+        username: 'test.name',
+        password: 'wrongpass'
+      }
+      @req_body = credentials.to_json
+      req_header = { 'CONTENT_TYPE' => 'application/json' }
+      post '/api/v1/accounts/authenticate', @req_body, req_header
+      _(last_response.status).must_equal 403
+    end
+  end
 end
