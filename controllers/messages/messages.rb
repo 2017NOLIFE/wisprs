b078d3cb9d3d89ccb1b3ea194dc12c07e2c16cb1
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-05-17 16:45:27 +0800

    Controllers refactored to use the auth key

--- controllers/messages/messages.rb
+++ controllers/messages/messages.rb
@@ -2,6 +2,16 @@ require 'sinatra'
 
 # /api/v1/messages routes
 class WispersBase < Sinatra::Base
+  def authorized_affiliated_message(env, project_id)
+    account = authenticated_account(env)
+    #all_projects = FindAllAccountProjects.call(id: account['id'])
+    #all_projects.select { |proj| proj.id == project_id.to_i }.first
+    nil
+  rescue => e
+    logger.error "ERROR finding project: #{e.inspect}"
+    nil
+  end
+
   get '/api/v1/messages/?' do
     content_type 'application/json'
 
@@ -13,30 +23,14 @@ class WispersBase < Sinatra::Base
 
     message_id = params[:id]
 
-    message = Message.where(id: message_id).first
+    message = Message.where(id: message_id).first #authorized_affiliated_message(env, message_id)
 
     if message
-      JSON.pretty_generate(data: message)
+      message.to_json
     else
       error_msg = "FAILED TO GET MESSAGE: \"#{message_id}\""
       logger.info error_msg
-      halt 404, error_msg
+      halt 401, error_msg
     end
-  end
-
-  post '/api/v1/messages/?' do
-    content_type 'application/json'
-
-    begin
-      message_info = JsonRequestBody.parse_symbolize(request.body.read)
-      new_message = SendMessage.call(message_info)
-    rescue => e
-      error_msg = "FAILED to create a new message: #{e.inspect}"
-      logger.info error_msg
-      halt 400, error_msg
-    end
-
-    status 201
-    headers('Location' => [@request_url.to_s, new_message.id].join('/'))
   end
 end
