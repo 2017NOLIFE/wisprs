7f977fb11902e1b11832d08b2f3a685e27f008dd
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-05-19 18:27:42 +0800

    Models updated with chat

--- models/accounts.rb
+++ models/accounts.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# Migration for Chat table
+
 require 'json'
 require 'base64'
 require 'sequel'
@@ -6,49 +10,46 @@ require_relative '../lib/secure_db'
 
 # Holds an Account's Information
 class Account < Sequel::Model
-	set_allowed_columns :username, :email
+  set_allowed_columns :username, :email
 
-	one_to_one :public_key, class: :Public_key, key: :owner_id
-	one_to_many :sent_messages, class: :Message, key: :from_id
-	one_to_many :received_messages, class: :Message, key: :to_id
+  one_to_one :public_key, class: :Public_key, key: :owner_id
 
-	plugin :timestamps, update_on_create: true
-	plugin :association_dependencies, public_key: :destroy
-	plugin :association_dependencies, sent_messages: :destroy
-	plugin :association_dependencies, received_messages: :destroy
+  one_to_many :sender_chats, class: :Chat, key: :sender_id
+  one_to_many :receiver_chats, class: :Chat, key: :receiver_id
+  one_to_many :sent_messages, class: :Message, key: :from_id
+  one_to_many :received_messages, class: :Message, key: :to_id
 
+  plugin :timestamps, update_on_create: true
+  plugin :association_dependencies, public_key: :destroy
+  plugin :association_dependencies, sender_chats: :destroy
+  plugin :association_dependencies, receiver_chats: :destroy
+  plugin :association_dependencies, sent_messages: :destroy
+  plugin :association_dependencies, received_messages: :destroy
 
-	# encrypt field data functions
-
-
-	def password=(new_password)
-		new_salt = SecureDB.new_salt
+  def password=(new_password)
+    new_salt = SecureDB.new_salt
     hashed = SecureDB.hash_password(new_salt, new_password)
     self.salt = new_salt
     self.password_hash = hashed
-	end
-
-	# decrypt field data functions
-
+  end
 
 # Json string
 
-	def to_json(options = {})
+  def to_json(options = {})
     JSON({
-						type: 'account',
+            type: 'account',
             id: id,
-           	attributes: {
+             attributes: {
               username: username,
               email: email,
               salt: salt
-						}
+            }
          },
          options)
-	end
+  end
 
-	def password?(try_password)
+  def password?(try_password)
     try_hashed = SecureDB.hash_password(salt, try_password)
     try_hashed == password_hash
   end
-
 end
---
+++ models/chat.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Migration for Chat table
+
+require 'json'
+require 'base64'
+require 'sequel'
+
+# Holds an Chat's Information
+class Chat < Sequel::Model
+  many_to_one :sender, class: :Account
+  many_to_one :receiver, class: :Account
+  one_to_many :messages, class: :Message, key: :chat_id
+
+  #plugin :timestamps, update_on_create: true
+  plugin :association_dependencies, messages: :destroy
+  #plugin :association_dependencies, sender_chats: :destroy
+  #plugin :association_dependencies, receiver_chats: :destroy
+
+end
