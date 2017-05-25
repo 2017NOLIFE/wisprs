dff0d21896862a82649474c121fac26b98320b19
ismaelnoble <ismaelnoble@gmail.com>
2017-05-25 22:53:09 +0800

    message policy as temp function untill refactor and init.rb for policies

---
+++ policies/message_policy_scope.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# Policy to determine if account can view a chat
+class MessagePolicy
+  # Scope of project policies
+  class Scope
+    def initialize(current_account, target_account)
+      @scope = all_messages(target_account)
+      @current_account = current_account
+      @target_account = target_account
+    end
+
+    def viewable
+      if @current_account == @target_account
+        @scope
+      else
+        @scope.select { |message| includes_receiver?(message, @current_account) }
+      end
+    end
+
+    private
+
+    def all_messages(account)
+      account.sent_messages + account.received_messages
+    end
+    # this may be redundant, also need to refactor for chats to clean this up
+    def includes_receiver?(message, account)
+      message.receiver.include? account
+    end
+  end
+end
